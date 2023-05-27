d92a1303a548911b4b47b4edc679d1971b917617
alexchristison <alexchristison@gmail.com>
2023-05-27 17:48:56 -0400

    finch1

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,4 +3,6 @@ from . import views
 	
 urlpatterns = [
     path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('sneakers/', views.sneakers_index, name='index') # can also be name='sneakers_index' BUT doesn't have to be
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
 
+# Baby step - usually a Model is used 
+sneakers = [
+  {'name': 'Air Jordan IV', 'brand': 'Nike', 'description': 'blue and white', 'year': 1989},
+  {'name': 'Air Tech Challenge 2', 'brand': 'Nike', 'description': 'Aka - Air Agassi. Hot Lava. ', 'year': 1990},
+]
 # Create your views here.
 # Define the home view
 def home(request):
   # Include an .html file extension - unlike when rendering EJS templates
   return render(request, 'home.html')
+
+def about(request):
+  return render(request, 'about.html')
+
+def sneakers_index(request):
+  return render(request, 'sneakers/index.html', {
+    'sneakers': sneakers
+  }) # see comment related in urls.py re: index vs sneakers_index
